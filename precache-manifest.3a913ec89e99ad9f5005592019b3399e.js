f2c666fe704c3a3bc7303b2e60cbfc41c484db05
ShevchukVA <60571733+ShevchukVA@users.noreply.github.com>
2020-11-03 20:58:10 +0200

    Updates

---
+++ precache-manifest.3a913ec89e99ad9f5005592019b3399e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac2cb2c8aa962cc8fe99b6c6a44a275",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "bb1db6ec3a0b2903837b",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.db30c346.chunk.css"
+  },
+  {
+    "revision": "0ccb472e47a1f31b28cf",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.04cf2e6f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.04cf2e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1db6ec3a0b2903837b",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.1b8cdb6f.chunk.js"
+  },
+  {
+    "revision": "eb1f7af60adc1370f746",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.de893a73.js"
+  }
+]);
